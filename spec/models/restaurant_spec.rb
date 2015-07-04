23f71352eba50f5235b429e707a16956d4d92530
Raymond Sapida <ray.sapida@gmail.com>
2015-07-03 21:31:41 -0700

    Add presence validations for restaurant fields

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of :name}
+  it {should validate_presence_of :address}
+  it {should validate_presence_of :phone}
+  it {should validate_presence_of :website}
 end
