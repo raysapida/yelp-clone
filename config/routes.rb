cfdce0ea8e9b62e1ec9335d75a6083fa8480be06
Raymond Sapida <ray.sapida@gmail.com>
2015-07-04 23:53:48 -0700

    Remove review index and show actions and assoicated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reviews
+  resources :reviews, except: [ :show, :index ]
   devise_for :users
   root to: 'restaurants#index'
   resources :restaurants
--- spec/routing/reviews_routing_spec.rb
+++ spec/routing/reviews_routing_spec.rb
@@ -3,16 +3,8 @@ require "rails_helper"
 RSpec.describe ReviewsController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/reviews").to route_to("reviews#index")
-    end
-
     it "routes to #new" do
       expect(:get => "/reviews/new").to route_to("reviews#new")
-    end
-
-    it "routes to #show" do
-      expect(:get => "/reviews/1").to route_to("reviews#show", :id => "1")
     end
 
     it "routes to #edit" do
