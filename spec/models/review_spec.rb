4f4f9abd9b51e5f119dbf1a2ebe7e4b85222d75a
Raymond Sapida <ray.sapida@gmail.com>
2015-07-05 16:23:28 -0700

    Add validations for review and user models

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   it { should belong_to :user }
   it { should belong_to :restaurant }
+  it { should validate_presence_of :rating }
+  it { should validate_numericality_of(:rating).only_integer
+       .is_greater_than_or_equal_to(1).is_less_than_or_equal_to(5)
+       .with_message("can only be a whole number between 1 and 5") }
 end
