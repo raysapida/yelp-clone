5021ea16e8432fa75a476534ce10d5042279c05f
Raymond Sapida <ray.sapida@gmail.com>
2015-08-14 16:20:26 -0700

    Add format tests for phone, website and address

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Restaurant, type: :model do
   it {should validate_presence_of :phone}
   it {should validate_presence_of :website}
   it {should validate_presence_of :image}
+  it {should_not allow_value('12 456 7890').for(:phone)}
+  it {should_not allow_value('example.com').for(:website)}
+  it {should_not allow_value('123 Somewhere Street').for(:address)}
+
   it {should have_many :reviews}
 end
